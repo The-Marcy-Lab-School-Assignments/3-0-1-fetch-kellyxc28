3937bd2ed8fd8790d742366feb62609af88dd3aa
kellyxc28 <kellyxiongc@gmail.com>
2024-06-12 17:17:37 -0400

    Completed Question 1: checkResponseStatus()

--- src/fetch-functions.js
+++ src/fetch-functions.js
@@ -1,13 +1,17 @@
-const userUrl = 'https://jsonplaceholder.typicode.com/users'
+const userUrl = 'https://jsonplaceholder.typicode.com/users';
 
 export const checkResponseStatus = () => {
+  return fetch(userUrl).then((response) => {
+    return {
+      status: response.status,
+      ok: response.ok,
+      url: response.url,
+    };
+  });
 };
 
-export const getUsers = () => {
-};
+export const getUsers = () => {};
 
-export const getUserPosts = () => {
-};
+export const getUserPosts = () => {};
 
-export const createNewUser = () => {
-}
+export const createNewUser = () => {};

1a32a1208ac33a90657469276756acbf784d170f
kellyxc28 <kellyxiongc@gmail.com>
2024-06-12 18:54:06 -0400

    Completed Question 4: createNewUser()

--- src/fetch-functions.js
+++ src/fetch-functions.js
@@ -30,4 +30,28 @@ export const getUserPosts = (userId, maxNumPosts = 3) => {
     });
 };
 
-export const createNewUser = (username, email) => {};
+export const createNewUser = (newUserData = { username, email }) => {
+  const postOption = {
+    method: 'POST', // The type of HTTP request
+    body: JSON.stringify(newUserData), // The data to be sent to the API
+  };
+
+  return fetch(userUrl, postOption)
+    .then((response) => {
+      if (!response.ok) {
+        return console.log(
+          `Fetch failed. ${response.status} ${response.statusText}`
+        );
+      }
+      return response.json();
+    })
+    .then((responseData) => {
+      // A POST request will return the object created with an id and a createdAt timestamp
+      console.log('Here is your data:', responseData);
+      return responseData;
+    })
+    .catch((error) => {
+      console.log('Error caught!');
+      console.error(error.message);
+    });
+};

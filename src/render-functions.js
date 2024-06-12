ecabc89b54d4e31edc9436973ce729e7b83dea01
kellyxc28 <kellyxiongc@gmail.com>
2024-06-12 19:21:44 -0400

    Completed Question 5: renderStatus()

--- src/render-functions.js
+++ src/render-functions.js
@@ -1,5 +1,5 @@
 export const setupPageBasics = (parentEl) => {
-    parentEl.innerHTML = `
+  parentEl.innerHTML = `
       <h1>Intro To Fetch!</h1>
       <div id='status'></div>
       <div id='users'>
@@ -21,23 +21,35 @@ export const setupPageBasics = (parentEl) => {
       <div id='new-user'></div>
     `;
 
-    const statusDiv = parentEl.querySelector('#status');
-    const usersUl = parentEl.querySelector('#users-list');
-    const postsUl = parentEl.querySelector('#posts-list');
-    const newUserForm = parentEl.querySelector('#new-user-form');
-    const newUserDiv = parentEl.querySelector('#new-user');
+  const statusDiv = parentEl.querySelector('#status');
+  const usersUl = parentEl.querySelector('#users-list');
+  const postsUl = parentEl.querySelector('#posts-list');
+  const newUserForm = parentEl.querySelector('#new-user-form');
+  const newUserDiv = parentEl.querySelector('#new-user');
 
-    return { statusDiv, usersUl, postsUl, newUserForm, newUserDiv };
+  return { statusDiv, usersUl, postsUl, newUserForm, newUserDiv };
 };
 
-export const renderStatus = () => {
-}
+export const renderStatus = (statusDiv, statusInfoObj) => {
+  const div = statusDiv;
 
-export const renderUsers = () => {
+  const h2 = document.createElement('h2');
+  h2.setAttribute('id', 'status-heading');
+  h2.textContent = `Info on - ${statusInfoObj.url}`;
+
+  const p = document.createElement('p');
+  p.setAttribute('id', 'status-code');
+  if (statusInfoObj.ok) {
+    p.textContent = `Status code: ${statusInfoObj.status}, OK!`;
+  } else {
+    p.textContent = `Status code: ${statusInfoObj.status}, FAIL!`;
+  }
+
+  div.append(h2, p);
 };
 
-export const renderPosts = () => {
-}
+export const renderUsers = () => {};
 
-export const renderNewUser = () => {
-}
+export const renderPosts = () => {};
+
+export const renderNewUser = () => {};
